bd444291160ff450dca83b943d9307d9bc61e30d
Miguel-Paganini <miguelbpaganini@hotmail.com>
2026-06-09 13:50:26 -0300

    Pagina main funcionando com os estilos e js nas pastas static

--- handlink/forms/auth.py
+++ handlink/forms/auth.py
@@ -1,59 +1,67 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from validate_docbr import CPF
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
 
 class SignUpForm(FlaskForm):
     name = StringField(
-        'Nome Completo',
+        "Nome Completo",
         validators=[DataRequired()],
-        render_kw={"placeholder": "João da Silva"}
-        )
-    
+        render_kw={"placeholder": "João da Silva"},
+    )
+
     email = StringField(
-        'E-mail',
-        validators=[DataRequired(),
-                     Email(
-                        message='Por favor, insira um endereço de e-mail válido.',
-                        check_deliverability=True
-                     )],
-        render_kw={"placeholder": "nome@exemplo.com"}
-        )
-    
+        "E-mail",
+        validators=[
+            DataRequired(),
+            Email(
+                message="Por favor, insira um endereço de e-mail válido.",
+                check_deliverability=True,
+            ),
+        ],
+        render_kw={"placeholder": "nome@exemplo.com"},
+    )
+
     password = PasswordField(
-        'Senha',
-        validators=[DataRequired(), Length(min=8)]
-        )
-    
+        "Senha",
+        validators=[DataRequired(), Length(min=8)],
+    )
+
     confirm_password = PasswordField(
-        'Confirmar Senha',
-        validators=[DataRequired(), EqualTo('password', message='As senhas devem coincidir')]
-        )
+        "Confirmar Senha",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="As senhas devem coincidir"),
+        ],
+    )
 
     cpf = StringField(
-        'CPF',
+        "CPF",
         validators=[DataRequired(), Length(min=11, max=11)],
-        render_kw={"placeholder": "00000000000"}
-        )
-    
+        render_kw={"placeholder": "00000000000"},
+    )
+
     def validate_cpf(self, field):
         cpf = field.data
         if not cpf.isdigit():
-            raise ValidationError('O CPF deve conter apenas números.')
-        
+            raise ValidationError("O CPF deve conter apenas números.")
+
         validator = CPF()
         if not validator.validate(cpf):
-             raise ValidationError('CPF inválido.')
+            raise ValidationError("CPF inválido.")
 
-    submit = SubmitField('Cadastrar')
+    submit = SubmitField("Cadastrar")
 
 
 class LoginForm(FlaskForm):
     email = StringField(
-        'E-mail',
+        "E-mail",
         validators=[DataRequired(), Email()],
-        render_kw={"placeholder": "nome@exemplo.com"}
+        render_kw={"placeholder": "nome@exemplo.com"},
     )
     password = PasswordField(
-        'Senha', validators=[DataRequired(), Length(min=8)])
-    submit = SubmitField('Login')
+        "Senha",
+        validators=[DataRequired(), Length(min=8)],
+    )
+    submit = SubmitField("Login")
